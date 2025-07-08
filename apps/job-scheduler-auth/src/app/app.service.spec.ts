b4a2afa6659957e49ff05ee1a7d5b292cfcdbb61
Xiaoran Meng <Xiaoran.Meng@teranet.ca>
2025-07-08 10:48:15 -0500

    Update app service test and CI workflow file

--- apps/job-scheduler-auth/src/app/app.service.spec.ts
+++ apps/job-scheduler-auth/src/app/app.service.spec.ts
@@ -13,8 +13,8 @@ describe('AppService', () => {
   });
 
   describe('getData', () => {
-    it('should return "Hello API"', () => {
-      expect(service.getData()).toEqual({ message: 'Hello API' });
+    it('should return "Nest API"', () => {
+      expect(service.getData()).toEqual({ message: 'Nest API' });
     });
   });
 });
